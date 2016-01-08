93ee03d9143975c4331cffe568dc81149f2a49f1
Alex Robertson <alexrson@gmail.com>
2016-01-08 12:46:24 -0800

    update current date

--- jCal/AppDelegate.swift
+++ jCal/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  jCal
 //
 //  Created by Alexander Robertson on 12/23/14.
-//  Copyright (c) 2014 Alexander Robertson. All rights reserved.
+//  Copyright (c) 2016 Alexander Robertson. All rights reserved.
 //
 
 import UIKit
